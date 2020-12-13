4dde29861469a9c01d83d080da55e80115b85bff
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-12-13 23:56:32 +0900

    seed-add

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,4 +2,5 @@ class Type < ApplicationRecord
 	has_many :centers
 	has_many :harmonics
 	has_many :horneys
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_140407) do
+ActiveRecord::Schema.define(version: 2020_12_09_145352) do
 
   create_table "centers", force: :cascade do |t|
     t.string "center_type", null: false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_12_04_140407) do
   create_table "types", force: :cascade do |t|
     t.string "type", null: false
     t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "user_type"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
